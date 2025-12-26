1f6d670ee7a6fbe3af05bbcb0b1515422b83cd9c
Arnold <arnold.gallegosm@gmail.com>
2025-12-25 20:40:54 -0500

    Actualizacion del build

--- build.sh
+++ build.sh
@@ -2,6 +2,12 @@
 # exit on error
 set -o errexit
 
+# Install Node.js dependencies
+npm install
+
+# Build Tailwind CSS
+npm run build:css
+
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
