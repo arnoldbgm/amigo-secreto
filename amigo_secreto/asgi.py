6dfb01023adcde801892d2d580eafb5bbbcadc17
Arnold <arnold.gallegosm@gmail.com>
2025-12-25 20:35:49 -0500

    Setting listo

--- amigo_secreto/asgi.py
+++ amigo_secreto/asgi.py
@@ -9,18 +9,23 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
 
 import os
 
+from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
-import core.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'amigo_secreto.settings')
 
+# Initialize Django ASGI application early to ensure the AppRegistry
+# is populated before importing code that may import ORM models.
+http_application = get_asgi_application()
+
+import core.routing
+
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    "websocket": URLRouter(
+    "http": http_application,
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
             core.routing.websocket_urlpatterns
         )
-    ,
+    ),
 })
-
